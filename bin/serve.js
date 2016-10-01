a22c5a83bd8b76ef04dd775f785b514ec71658a7
Poga Po <poga.bahamut@gmail.com>
2016-10-02 05:29:52 +0800

    fix serving multiple feed

--- bin/serve.js
+++ bin/serve.js
@@ -17,19 +17,21 @@ try {
 
 var servers = []
 config.feeds.forEach(conf => {
-  opts.key = keystore[conf.url]
-
-  servers.push((cb) => {
-    server.serve(conf.url, opts, cb)
-  })
+  servers.push(serve(conf))
 })
+
+function serve (conf) {
+  return (cb) => {
+    server.serve(conf.url, Object.assign({}, opts, {key: keystore[conf.url]}), cb)
+  }
+}
 
 async.series(servers, (err, connections) => {
   if (err) throw (err)
   connections.forEach(conn => {
-    console.log(conn.feed.key().toString('hex'))
+    console.log('serving', conn.url, 'at', conn.feed.key().toString('hex'))
     if (!keystore[conn.url]) keystore[conn.url] = conn.feed.key().toString('hex')
   })
 
-  fs.writeFileSync(`${argv._[0]}.manifest`, JSON.stringify(keystore))
+  fs.writeFileSync(`${argv._[0]}.manifest`, JSON.stringify(keystore, undefined, 2))
 })

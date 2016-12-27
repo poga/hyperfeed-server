8a52aba064b02d4d522aee0154c8bf75803bfc08
Poga Po <poga.bahamut@gmail.com>
2016-12-28 00:56:28 +0800

    use feed-hyperify

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
-const request = require('request')
+#! /usr/bin/env node
+const fs = require('fs')
+const async = require('async')
+const level = require('level')
+const hyperfeed = require('hyperfeed')
+const hyperdrive = require('hyperdrive')
+const Hyperify = require('feed-hyperify')
 const hyperdiscovery = require('hyperdiscovery')
 
-module.exports = {serve: serve, swarm: swarm}
+const argv = require('minimist')(process.argv.slice(2))
+const configPath = argv._[0]
+const manifestPath = `${configPath}.manifest`
+const dbPath = `${configPath}.db`
 
-function serve (feed, url, opts, cb) {
-  request(url, (err, resp, body) => {
-    if (err) return done(err)
+var config = JSON.parse(fs.readFileSync(configPath))
+var keystore = safeLoad(manifestPath)
+var hf = hyperfeed(hyperdrive(level(dbPath)))
 
-    feed.update(body).then(() => { done() })
-  })
+var servers = []
+config.feeds.forEach(conf => {
+  servers.push(serve(conf))
+})
 
-  function done (err) {
-    if (err) return cb(err)
-    var sw = swarm(feed, opts.useWebRTC)
-    cb(null, {url: url, feed: feed, sw: sw})
+// start crawler for each feed sequentially
+async.series(servers, (err, connections) => {
+  if (err) throw (err)
+
+  if (argv.merge) {
+    var out = hf.createFeed(keystore['merge'], {own: true})
+    var ins = connections.map(c => c.feed)
+    require('hyperfeed-merge')(ins, out)
+    debug(out, hyperdiscovery(out))
+
+    keystore['merge'] = out.key.toString('hex')
+    console.log('merged feed key:', out.key.toString('hex'))
   }
 
-  if (opts.live) {
-    setInterval(() => {
-      request(url, (err, resp, body) => {
-        if (err) return console.log(err)
+  fs.writeFileSync(manifestPath, JSON.stringify(keystore, undefined, 2))
+})
 
-        feed.update(body).then(() => { console.log(`[${feed.key().toString('hex')}]`, 'updated') })
-      })
-    }, opts.interval)
+function serve (conf) {
+  return (cb) => {
+    var feed = hf.createFeed(keystore[conf.url], {own: true, scrap: conf.scrap})
+    var crawler = new Hyperify(feed, conf.url, 300 * 1000)
+    var sw = hyperdiscovery(feed)
+    debug(feed, sw)
+    if (!keystore[conf.url]) keystore[conf.url] = feed.key.toString('hex')
+    console.log('serving', conf.url, 'at', feed.key.toString('hex'))
+    cb(null, {crawler, sw, feed})
   }
 }
 
-function swarm (feed) {
-  return hyperdiscovery(feed)
+function debug (feed, sw) {
+  sw.on('connection', function (peer, type) {
+    console.log(`[${feed.key.toString('hex')}]`, 'got', type) // type is 'webrtc-swarm' or 'discovery-swarm'
+    console.log(`[${feed.key.toString('hex')}]`, 'connected to', sw.connections, 'peers')
+    peer.on('close', function () {
+      console.log(`[${feed.key.toString('hex')}]`, 'peer disconnected')
+    })
+  })
+}
+
+function safeLoad (path) {
+  try {
+    return JSON.parse(fs.readFileSync(path))
+  } catch (e) {
+    // ignore if keystore file is not found
+    return {}
+  }
 }
